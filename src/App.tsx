779343772ca3ee9487b0a2a225855cd0415ce7bb
Danny123456789121 <kurthdaniel@web.de>
2024-01-31 23:33:02 +0100

    add google maps img

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import './App.css';
+import InputForm from './components/InputForm.tsx';
+import { useState } from 'react';
+import { Store } from 'antd/lib/form/interface';
 
-function App() {
-  return <></>;
-}
+const App = () => {
+  const [imageURL, setImageURL] = useState<string | undefined>();
+
+  const onCustomFinish = async (values: Store) => {
+    const response = await fetch(
+      `https://maps.googleapis.com/maps/api/staticmap?center=${values.Latitude},${values.Longitude}&zoom=${values.Zoom}&size=2048x2048&maptype=satellite&key=${import.meta.env.VITE_GMP_API_KEY}`,
+    );
+    const blob = await response.blob();
+    setImageURL(URL.createObjectURL(blob));
+  };
+
+  return (
+    <>
+      <InputForm onCustomFinish={onCustomFinish} />
+      {imageURL && (
+        <img
+          src={imageURL}
+          width="512px"
+          height="512px"
+          style={{ border: '0px' }}
+          alt=""
+        />
+      )}
+    </>
+  );
+};
 
 export default App;
